e53be45aec971a69ddbc306f09ad7b4f8eb25234
Riya-Agrawal950 <riyaagarwal486@gmail.com>
2024-07-19 15:08:26 +0530

    added

---
+++ src/FunctionalComponent/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Footer(){ 
+    return (
+      <>
+      <div className='bg-primary text-center text-light p-2'>
+        <p>copyright@newsapp.com</p>
+      </div>
+      </>
+    )
+  }
+
